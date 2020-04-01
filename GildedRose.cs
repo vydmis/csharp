bd5fc61c348db5c29de1308405ef4fd86c392dcb
vydmis <vydmis@gmail.com>
2020-04-01 14:59:49 +0300

    Initial commit

---
+++ GildedRose.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+
+namespace csharp
+{
+    public class GildedRose
+    {
+        IList<Item> Items;
+        public GildedRose(IList<Item> Items)
+        {
+            this.Items = Items;
+        }
+
+        public void UpdateQuality()
+        {
+            // Refactored UpdateQuality method
+
+            foreach (Item item in Items)
+            {
+                updateItem(item);
+            }
+        }
+
+        public void updateItem(Item item)
+        {
+            //Created new method updateItem
+
+            if (item.Name == "Aged Brie")
+            {
+                AgedBrieProcessor(item);
+            }
+            else if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                BackstageProcessor(item);
+            }
+            else if (item.Name == "Sulfuras, Hand of Ragnaros")
+            {
+                SulfurasProcessor(item);
+            }
+            else if (item.Name == "Conjured Mana Cake")
+            {
+                ConjuredProcessor(item);
+            }
+            else 
+            {
+                StandartProcessor(item);
+            }
+            
+        }
+
+        private Item AgedBrieProcessor(Item item)
+        {
+            // Created method for each property
+
+            if (--item.SellIn < 0)
+            {
+                item.Quality += 2;
+            }
+            else
+            {
+                item.Quality++;
+            }
+
+            if (item.Quality > 50)
+            {
+                item.Quality = 50;
+            }
+
+            return item;
+        }
+
+        private Item BackstageProcessor(Item item)
+        {
+            // Created method for each property
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+            else if (item.SellIn < 5)
+            {
+                item.Quality += 3;
+            }
+            else if (item.SellIn < 10)
+            {
+                item.Quality += 2;
+            }
+            else 
+            {
+                item.Quality ++;
+            }
+
+            if (item.Quality > 50)
+            {
+                item.Quality = 50;
+            }
+
+            return item;
+        }
+
+        private Item SulfurasProcessor(Item item)
+        {
+            // Created method for each property
+
+            //emty function on this moment
+
+            return item;
+        }
+
+        private Item StandartProcessor(Item item)
+        {
+            // Created method for each property
+
+            if (--item.SellIn < 0)
+            {
+                item.Quality -= 2;
+            }
+            else 
+            {
+                item.Quality--;
+            }
+
+            if (item.Quality < 0)
+            {
+                item.Quality = 0;
+            }
+
+            return item;
+        }
+
+        private Item ConjuredProcessor(Item item)
+        {
+            // Created method for each property
+
+            if (--item.SellIn < 0)
+            {
+                item.Quality -= 4;
+            }
+            else
+            {
+                item.Quality -= 2;
+            }
+
+            if (item.Quality < 0)
+            {
+                item.Quality = 0;
+            }
+
+            return item;
+        }
+    }
+}
---
+++ GildedRoseTest.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace csharp
+{
+    [TestFixture]
+    public class GildedRoseTest
+    {
+        //Created simple test
+        [Test]
+        public void foo()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
+
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+
+            Items[0].SellIn.Should().Be(9);
+            Items[1].SellIn.Should().Be(1);
+            Items[2].SellIn.Should().Be(4);
+            Items[3].SellIn.Should().Be(0);
+            Items[4].SellIn.Should().Be(14);
+            Items[5].SellIn.Should().Be(2);
+
+            Items[0].Quality.Should().Be(19);
+            Items[1].Quality.Should().Be(1);
+            Items[2].Quality.Should().Be(6);
+            Items[3].Quality.Should().Be(80);
+            Items[4].Quality.Should().Be(21);
+            Items[5].Quality.Should().Be(4);
+        }
+    }
+}
